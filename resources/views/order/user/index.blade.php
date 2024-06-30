c2c1f1cf37b924bfc00113f5516045c52ddbeda4
Lazkerz <Laziranurul@gmail.com>
2024-06-30 18:17:18 +0700

    feat: update tampilan

--- resources/views/order/user/index.blade.php
+++ resources/views/order/user/index.blade.php
@@ -8,10 +8,11 @@
     <div class="flex flex-row overflow-hidden justify-center gap-3">
         @foreach ($orders as $order)
         <div class="w-fit flex flex-row gap-2 border rounded-lg p-4 text-left bg-white shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:scale-105">
-            <img src="{{ asset($order->paket->gbr_paket) }}" alt="{{ $order->paket->nama_paket }}" class="w-[150px] h-[100px] object-cover object-top">
+            <img src="{{ asset($order->paket->gbr_paket) }}" alt="{{ $order->paket->nama_paket }}" class="w-[150px] h-[150px] object-cover object-top">
             <div class="flex flex-col justify-start items-startr">
                 <h2 class="text-xl font-semibold text-amber-500">{{ $order->paket->nama_paket }}</h2>
                 <span class="bg-amber-400 text-black text-xs font-semibold p-1 w-fit rounded">{{ $order->status }}</span>
+                <span class="text-black text-xs font-semibold p-1 w-fit rounded">Perkiraan bulan: {{ $order->bulan }}</span>
                 @if ($order->status == 'Orderan Diterima')
                 <div class="mt-2">
                     <p class="text-gray-500 text-sm mb-2">Screenshot orderan dan kirim ke telegram</p>
